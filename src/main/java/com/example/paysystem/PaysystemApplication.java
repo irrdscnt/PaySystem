08becdcc50982fdef4c202f36ff631dabe3f8153
Aikanysh <lailievaa02@gmail.com>
2024-05-31 17:15:38 +0600

    initial commit

---
+++ src/main/java/com/example/paysystem/PaysystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.paysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaysystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaysystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/paysystem/PaysystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.paysystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaysystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
